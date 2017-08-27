8b001f1f8b881bfc27bcb5187a92ef42c2c6588e
Pedro Pires <pedro.pires@accenture.com>
2017-08-27 05:41:48 +0100

    Lesson 10, exercise 01 ...

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
@@ -1,3 +1,24 @@
-// TODO (5) Create a new class called SunshineSyncIntentService that extends IntentService
-//  TODO (6) Create a constructor that calls super and passes the name of this class
-//  TODO (7) Override onHandleIntent, and within it, call SunshineSyncTask.syncWeather
+package com.example.android.sunshine.sync;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+public class SunshineSyncIntentService extends IntentService {
+
+    // TODO (5) Create a new class called SunshineSyncIntentService that extends IntentService
+    //  TODO (6) Create a constructor that calls super and passes the name of this class
+    //  TODO (7) Override onHandleIntent, and within it, call SunshineSyncTask.syncWeather
+
+//    public static final String ACTION = "action-sync-data";
+
+    public SunshineSyncIntentService() {
+        super(SunshineSyncIntentService.class.getSimpleName());
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        SunshineSyncTask.syncWeather(this);
+    }
+
+}
--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,4 +1,49 @@
+package com.example.android.sunshine.sync;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.util.Log;
+
+import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
+public class SunshineSyncTask {
+
 //  TODO (1) Create a class called SunshineSyncTask
 //  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
 //      TODO (3) Within syncWeather, fetch new weather data
 //      TODO (4) If we have valid results, delete the old data and insert the new
+
+    private static final String TAG = SunshineSyncTask .class.getSimpleName();
+
+    public synchronized static void syncWeather(Context context) {
+
+        String weatherDataJson = null;
+        try {
+            weatherDataJson = NetworkUtils.getResponseFromHttpUrl(NetworkUtils.getUrl(context));
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e(TAG,"Error fetching weather from network",e);
+            return;
+        }
+
+        ContentValues[] values = null;
+        try {
+            values = OpenWeatherJsonUtils.getWeatherContentValuesFromJson(context,weatherDataJson);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.e(TAG,"Error parsing JSON data received from the network",e);
+            return;
+        }
+
+        ContentResolver contentResolver = context.getContentResolver();
+        contentResolver.delete(WeatherContract.WeatherEntry.CONTENT_URI,null,null);
+        contentResolver.bulkInsert(WeatherContract.WeatherEntry.CONTENT_URI,values);
+    }
+}
